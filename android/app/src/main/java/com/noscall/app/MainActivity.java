b18c7500991f3412309dda4b85be7e8eb8407ebd
sanah9 <2194395+sanah9@users.noreply.github.com>
2025-10-17 17:39:32 +0800

    improve Android UI

--- android/app/src/main/java/com/noscall/app/MainActivity.java
+++ android/app/src/main/java/com/noscall/app/MainActivity.java
@@ -3,9 +3,20 @@ package com.noscall.app;
 import io.flutter.embedding.android.FlutterFragmentActivity;
 import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodChannel;
+import android.view.WindowManager;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class MainActivity extends FlutterFragmentActivity {
     private static final String CHANNEL = "com.noscall.native_methods";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public void configureFlutterEngine(FlutterEngine flutterEngine) {
