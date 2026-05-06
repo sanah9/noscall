21a486f4a34aa9735f7da560d8b884b959569f24
sanah9 <2194395+sanah9@users.noreply.github.com>
2026-05-06 20:54:46 +0800

    fix ConnectivityResult  issue

--- lib/setting/widgets/setting_section_widgets.dart
+++ lib/setting/widgets/setting_section_widgets.dart
@@ -145,8 +145,6 @@ String networkStatusLabel(List<ConnectivityResult> connectivity) {
       return 'Other';
     case ConnectivityResult.bluetooth:
       return 'Bluetooth';
-    case ConnectivityResult.satellite:
-      return 'Satellite';
   }
 }
 
